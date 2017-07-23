889a5ea55a5dec30636861e7c9e3a385bad0cd52
hs <sth1pal@pal87498.attlocal.net>
2017-07-23 11:02:14 -0700

    cleaned up menu styles

--- dev/hsLibs/mithril/hsWidgets/src/hsMenu.ts
+++ dev/hsLibs/mithril/hsWidgets/src/hsMenu.ts
@@ -1,27 +1,49 @@
 /**
+ * # hsMenu
+ * Creates a simple menu with several items.
+ * The `Menu` object is passed into `Mithril`'s m function with a `MenuDesc` object that specifes the items,
+ * the currently selected item, and a `select` function that is called when the menu selection changes. 
  * 
+ * ## Example
+ * <code>
+    const desc:MenuDesc = {
+        items: ['One', 'Two', 'Three'],
+        selectedItem: 'One',
+        select: (item:string) => <do something>  
+    };
+    m(Menu, {desc: desc}); 
+ * </code>
  */
 
  /** */
 import { m, Vnode} from '../../mithril';
-import { Container, px, FILL } from '../../hsLayout/src/';
+import { Container } from '../../hsLayout/src/';
 
 /** passed into Menu from the calling application */
 export interface MenuDesc {
     /** the items on the menu */
-    items: string[],
+    items: string[];
     /** the currently selected item */
-    selectedItem: string,
+    selectedItem: string;
     /** the function to call when the selection changes */
-    select: (item:string) => void
+    select: (item:string) => void;
 }
 
+/** interface of the parameter passed to a `MenuItem` */
 interface MenuItemDesc {
-    title: string,
-    selected: boolean
-    select: (item:string) => void
+    /** the item's title */
+    title: string;
+    /** the item's select status */
+    selected: boolean;
+    /** the function to call if this item is selected */
+    select: (item:string) => void;
 }
+
+/**
+ * Creates a simple menu with several items, as configured by the desc:MenuDesc object passed as a parameter. 
+ */
 export class Menu extends Container {
+    /** instance variable, keeping a list of menu items and a `select` function for tracking which item is selected. */
     menu = { 
         items: {},
         select: (title:string) => {
@@ -35,7 +57,7 @@ export class Menu extends Container {
         node.attrs.desc = undefined;
 
         this.menu.select(desc.selectedItem);
-        return this.layout('.hs-module-title', node, { columns: <any[]>[] }, desc.items.map((l:string) => {
+        return this.layout('.hs-menu', node, { columns: <any[]>[] }, desc.items.map((l:string) => {
             if (!this.menu.items[l]) { this.menu.items[l] = {title: l, selected: false, select:(item:string) => {
                 this.menu.select(item); // local housekeeping: make sure the item's style shows correct selection
                 desc.select(item);      // trigger any actions formt he selection
@@ -45,13 +67,15 @@ export class Menu extends Container {
     }
 };
 
+/**
+ * Creates a menu item as part of the menu, as configured by the desc:MenuItemDesc object passed as a parameter.
+ */
 class MenuItem extends Container {
     view(node: Vnode): Vnode {
-        function selected() { desc.select(desc.title); }
-        const desc = node.attrs.desc;
+        const desc:MenuItemDesc = node.attrs.desc;
         node.attrs.desc = undefined;
-        return this.layout(`.hs-header-tab`, node, {
-                class: desc.selected?'hs-header-tab-selected': '',
+        return this.layout(`.hs-menu-item`, node, {
+                class: desc.selected?'hs-menu-item-selected': '',
                 onclick:() => { desc.select(desc.title); }, 
             }, [m('', desc.title)]
         );
