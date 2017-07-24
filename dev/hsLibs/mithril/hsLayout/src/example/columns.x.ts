0079fc03427db9a8aeb22c649b9baedbe40bf22e
hs <sth1pal@pal87498.attlocal.net>
2017-07-23 19:56:37 -0700

    added column example

---
+++ dev/hsLibs/mithril/hsLayout/src/example/columns.x.ts
@@ -0,0 +1,48 @@
+import { m, Vnode }            from '../../../mithril';
+import { Container }    from '../';
+
+
+class Columns extends Container {
+    view(node: Vnode): Vnode {
+        return this.layout(`.hs-column-example`, node, {rows:[]}, [
+            m(Columns1),
+            m(Columns2),
+            m(Columns3),
+            m(Columns4)
+        ]);
+    }
+};
+
+class Columns1 extends Container {
+    view(node: Vnode): Vnode {
+        return this.layout(`.hs-column-example`, node, {column:[]}, [
+            'one', 'two', 'three'
+        ]);
+    }
+};
+
+class Columns2 extends Container {
+    view(node: Vnode): Vnode {
+        return this.layout(`.hs-column-example`, node, {column:[]}, [
+            'one', 'two', 'three'
+        ]);
+    }
+};
+
+class Columns3 extends Container {
+    view(node: Vnode): Vnode {
+        return this.layout(`.hs-column-example`, node, {column:[]}, [
+            'one', 'two', 'three'
+        ]);
+    }
+};
+
+class Columns4 extends Container {
+    view(node: Vnode): Vnode {
+        return this.layout(`.hs-column-example`, node, {column:[]}, [
+            'one', 'two', 'three'
+        ]);
+    }
+};
+
+m.mount(document.body, Columns);
