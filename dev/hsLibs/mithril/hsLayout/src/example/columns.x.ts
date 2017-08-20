f5debea90b6cf3b7eea8e5efc16fb8f68d08881c
hs <sth1pal@pal87498.attlocal.net>
2017-08-19 17:26:58 -0700

    added layout examples

--- dev/hsLibs/mithril/hsLayout/src/example/columns.x.ts
+++ dev/hsLibs/mithril/hsLayout/src/example/columns.x.ts
@@ -1,48 +1,80 @@
-import { m, Vnode }            from '../../../mithril';
-import { Container }    from '../';
+import { m, Vnode }             from '../../../mithril';
+import { Container, HsConfig }  from '../';
 
 
-class Columns extends Container {
-    getComponents(node: Vnode): Vnode {
-        return m(`.hs-column-example`, {rows:[]}, [
-            m(Columns1),
-            m(Columns2),
-            m(Columns3),
-            m(Columns4)
-        ]);
+const myConfig = {
+    Rows: {
+        columns:  [''],
+        maxCount: 5,
+        leafCSS: 'leaf',
+        content: [
+            { Rows: {
+                rows: [''],
+                content: [
+                    { aLeaf: { css:'leaf', columns: ['']}},
+                    { aLeaf: { css:'leaf', columns: ['100px'] }},
+                    { aLeaf: { css:'leaf', columns: ['100px', '200px'] }},
+                    { aLeaf: { css:'leaf', columns: ['20%'] }},
+                    { aLeaf: { css:'leaf', columns: ['20%', 'fill'] }}
+                ]
+            }},
+            { Rows: {
+                columns: [''],
+                content: [
+                    { aLeaf: { css:'leaf', rows: ['']}},
+                    { aLeaf: { css:'leaf', rows: ['100px'] }},
+                    { aLeaf: { css:'leaf', rows: ['100px', '200px'] }},
+                    { aLeaf: { css:'leaf', rows: ['20%'] }},
+                    { aLeaf: { css:'leaf', rows: ['20%', 'fill'] }}
+                ]
+            }}
+        ]
+/*        
+    },
+    route: {
+        default: '/api',
+        paths: [
+            '/api',             // defines `http://localhost/#!/api/
+            '/api/:lib',        // defines `http://localhost/#!/api/:hsLib
+            '/api/:lib/:field'  // defines `http://localhost/#!/api/:hsLib/:id        
+        ]
+*/        
     }
 };
 
-class Columns1 extends Container {
-    getComponents(node: Vnode): Vnode {
-        return m(`.hs-column-example`, {column:[]}, [
-            'one', 'two', 'three'
-        ]);
+function next(fn:any) {
+    return setTimeout(() => {
+        fn();
+        next(fn);
+    }, 2000);
+}
+
+const example = { 
+    Rows: class extends Container {
+        getComponents(node: Vnode): Vnode {
+            node.attrs.content.forEach((c:any) => c.attrs.maxCount = node.attrs.maxCount);
+            return super.getComponents(node);
+        }
+    },
+    aLeaf: class extends Container {
+        count = 1;
+        maxCount = 0;
+
+        constructor() {
+            super();
+            next(() => {
+                this.count = (this.count >= this.maxCount)? 0 : this.count+1;
+                m.redraw();
+            });
+        }
+
+        getComponents(node: Vnode): Vnode {
+            this.maxCount = this.maxCount = node.attrs.maxCount || 3;
+            const dims = node.attrs.columns || node.attrs.rows;
+            const content = [...Array(this.count).keys()].map((c:number) => `${node.attrs.columns?'Columns':'Rows'}<br>${c+1}<br>[${dims.join()}]`);
+            return content;
+        }
     }
 };
 
-class Columns2 extends Container {
-    getComponents(node: Vnode): Vnode {
-        return m(`.hs-column-example`, {column:[]}, [
-            'one', 'two', 'three'
-        ]);
-    }
-};
-
-class Columns3 extends Container {
-    getComponents(node: Vnode): Vnode {
-        return m(`.hs-column-example`, {column:[]}, [
-            'one', 'two', 'three'
-        ]);
-    }
-};
-
-class Columns4 extends Container {
-    getComponents(node: Vnode): Vnode {
-        return m(`.hs-column-example`, {column:[]}, [
-            'one', 'two', 'three'
-        ]);
-    }
-};
-
-m.mount(document.body, Columns);
+new HsConfig([example]).attachNodeTree(myConfig, document.body);
