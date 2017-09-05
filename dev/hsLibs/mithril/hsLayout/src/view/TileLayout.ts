517ba53e64e0e8f75c5b0ed225461d066685a779
hs <sth1pal@pal87498.attlocal.net>
2017-09-04 21:50:32 -0700

    added tiles format for %

--- dev/hsLibs/mithril/hsLayout/src/view/TileLayout.ts
+++ dev/hsLibs/mithril/hsLayout/src/view/TileLayout.ts
@@ -1,41 +1,74 @@
-import { Container }    from './Container';
-import { Layout }       from './Layout';
-import { LayoutToken, DefinedToken, PixelToken }    from './Tokens';
-import { Vnode}         from '../../../mithril';
-
 /**
- * TileLayout. Provides functionality to create tile layouts.
- */
+Lays out components in tiles
+Use 
+- `{tiles: [attributes]}`
 
-/**
- * contains style settings for the row and column layout
- */
-const cParams = {
-    cssClass: '.hs-column-layout',
-    fields: ['top', 'bottom', 'left', 'right', 'height', 'width']
-};
+to invoke this layout.
 
-type descriptor = {size:number, code:string, fields:{}};
-
-/**
-Lays its components out in tiles.
-Use `{tiles: {attributes}` to invoke this layout.
-See [Examples][example]
-
-[example]: ./example/layout.html
 ### Example
 <code>
-    {tiles: [px(200), FILL]} // --> top row has height 200px, all other rows evenly share remaining space 
+    {tiles: ["20%]}   // --> tiles will all have 20% of available height and width 
 </code>
+ * <example>
+ * <file name='script.js'>
+ * let c = [];
+ * function next() {
+ *     if (c.length >= 9) { c = []; }
+ *     else { c.push(''); }
+ *     setTimeout(next, 2000);
+ *     m.redraw();
+ * }
+ * 
+ * m.mount(root, { 
+ *     view:() => m(layout.Container, {
+ *         tiles:['30%','fill'], 
+ *         content: c.map((c,i)=>(''+i)),
+ *         css: '.myExample'
+ *     })
+ * });
+ * next();
+ * </file>
+ * <file name='style.css'>
+ * .myExample .hs-layout { 
+ *     border: 1px solid white;
+ *     background-color: #ccc;
+ *     color:       #a44; 
+ *     text-align:  center;
+ * }
+ * </file>
+ * </example>
 
 ## Attributes
+The following values **v** are valid entries in The Attributes array:
+- **px(n)** or **"<n>px"** -- a fixed number of pixels 
+- **pc(n)** or **"<n>%"**  -- a fixed percentage of available space
+- **FILL** or **"fill"**   -- a special constant to indicate - may appear only once per array.
+
+The following options are supported for the Attributes array:
+- **[ ]**: An empty array; The available tiles will cover the entire width and height. Their size will adapt 
+    as tiles are added
+- **[FILL]**: like [ ], except the last tile fills the available space. 
+- **[v]**: All components have the specified width and height (in px or %) and will fill the available space from the left,
+    leaving any remaining unused space on the right. 
+- **[w, h]**: All components have the specified width w and height h (in px or %) and will fill the available space from the left,
+    leaving any remaining unused space on the right. 
+- **[v, FILL]**: like [v], except that the last tile in each row will fill the remaining available width 
+    and the tiles in the bottom row will fill the remaining height
+- **[w, h, FILL]**: like [w, h], except that the last tile will fill the remaining available width. 
+ */
+/** */
+import { Container }    from './Container';
+import { Layout }       from './Layout';
+import { LayoutToken, FillToken, DefinedToken, PixelToken }    from './Tokens';
+import { Vnode}         from '../../../mithril';
+
+type descriptor = {top:string, left:string, right:string, bottom:string, width:string, height:string};
+
+/**
  */
 class Tiles extends Layout {
-    firstFixed: number; // number of DefinedToken entries at the beginning
-    lastFixed:  number; // number of DefinedToken entries at the end
-    unit:(num:number)=>descriptor[];
-    fields: string[];
     cssClass:string;
+    unit: any;
 
     /**
      * Constructs a Pillared layout (rows or columns)
@@ -44,115 +77,45 @@ class Tiles extends Layout {
      */
     constructor(public areaDesc:LayoutToken[]) { 
         super(areaDesc); 
-        this.fields = cParams.fields;
-        this.cssClass = cParams.cssClass;
-
-        let n = areaDesc.length-1;
-        let first = 0;
-        let last  = 0;        
         // if any of the dimensions are in px, use the pixel method; else use the percent method
-        // get index of first and last undefined area, if any            
-        this.unit = areaDesc.some((area:LayoutToken) => (area instanceof PixelToken))? this.unitPixel : this.unitPercent;   // true if any area is PixelToken
-        
-        // determine first = number of consecutive fixed fields at start
-        areaDesc.some((area:LayoutToken, i:number) => ((areaDesc[i]   instanceof DefinedToken)? ++first<0 : true));         
-
-        // determine last  = number of consecutive fixed fields at end
-        areaDesc.some((area:LayoutToken, i:number) => ((areaDesc[n-i] instanceof DefinedToken)? ++last<0  : true));         
-
-        this.firstFixed = first;
-        this.lastFixed  = Math.min(last, areaDesc.length-first);
+        // get unitPixel if any area is PixelToken           
+        this.unit = areaDesc.some((area:LayoutToken) => (area instanceof PixelToken))? 
+            this.unitPixel : this.unitPercent;           
     };
 
-    // num: number of areas to layout
-    /**
-     * Creates an iterable list of size descriptors, one for each area to be layed out.
-     * Each descriptor 
-     * @param num the number pof areas to be layed out
-     * @return Iterable list of size descriptors, one for each area to be layed out
-     */
-    private getSizes(num:number):descriptor[] {
-        const first = this.firstFixed;
-        const last  = this.lastFixed;
-        const desc  = this.areaDesc;
-        const len = desc.length;
-        function getSize(i:number):descriptor {
-            let size:number = null;
-            let t = null;
-            if (i > num-1-last)  { size = desc[len - (num-i)].getSize(); t = 'end'; }       // end sequence
-            else if (i < first)  { size = desc[i].getSize(); t = 'start'; }                 // start sequence
-            else if (len>0 && len===first){ size = desc[len-1].getSize(); t = 'start'; }    // all items 
-            return {size:size, code:t, fields:{}};
-        } 
-        return [...Array(num).keys()].map(getSize);
-    }
+    private unitPercent(num:number) {
+        const desc = this.areaDesc;
+        const fill = this.areaDesc.some(a => (a instanceof FillToken));
+        const root = Math.sqrt(num);
+        const rows = Math.round(root);
+        let   cols = Math.floor(root);
+        if (root > cols) { cols++; }
+        let width  = (desc[0] instanceof DefinedToken)? desc[0].getSize() : undefined;
+        let height = (desc[1] instanceof DefinedToken)? desc[1].getSize() : width;
 
-    private unitPercent(num:number):descriptor[] {
-        let f = this.fields;
-        let max = 100.0;
-        let defDim = max / num;      // divvy up remaining space
-        let styles:descriptor[] = this.getSizes(num);
+        width  = width  || 100/cols;
+        height = height || 100/rows;
+        let left = 0;
+        let top  = 0;
 
-        styles.forEach(style => { if (style.size) { max = max - style.size; num--; } });
 
-        function pass(styles:descriptor[], ix0:number, ix1:number, breakCond:(cond:string)=>boolean) {
-            let sumDim = 0;
-            styles.some(style => { // stop when breakCond is met
-                let size = style.size || defDim;
-                if (breakCond(style.code)) { return true; }
-                style.fields[f[ix0]] = sumDim+'%';
-                sumDim += size;
-                style.fields[f[ix1]] = (100-sumDim)+'%'; 
-                style.fields[f[5]] = 'auto';
-                return false;
-            });
-        }
-
-        pass(styles, 2, 3, (e:string) => e==='end');              // forward pass
-        pass(styles.reverse(), 3, 2, (e:string) => e!=='end');    // backward pass
-        return styles.reverse();    // reverse a second time for original sequence.
+        let styles = [...Array(num).keys()].map(i => { 
+            let r = 'auto';    let w = width+'%'; 
+            let b = 'auot';    let h = height+'%';
+            if ((left + 2*width) > 100 && fill) { r = '0%'; w = 'auto'; }
+            if ((top + 2*height) > 100 && fill) { b = '0%'; h = 'auto'; }
+            const style = `
+                top: ${Math.floor(top)}%; bottom:${b};
+                left: ${left}%;           right:${r};
+                width: ${w};              height: ${h};
+            `;
+            if ((left += width) > 100-width) { left = 0; top += height; }
+            return style;
+         });
+        return styles;    // reverse a second time for original sequence.
     };
 
-    private unitPixel(num:number):descriptor[] { // pattern: [px, px, FILL , px, px]
-        let styles:descriptor[] = this.getSizes(num);
-        let f = this.fields;
-       
-        let defDim = 100.0/num;          // used for unspecified widths
-
-        // work forwards through the heights
-        let sumDim = 0;
-        styles.some((style, i) => {
-            if (style.code==='start') {   // so far, all heights explicitly set as px
-                style.fields[f[2]] = sumDim +'px';
-                sumDim += style.size + (this.spacing || 0) + (this.spacing || 0);
-                style.fields[f[3]] = 'auto';
-                style.fields[f[5]] = style.size +'px';
-            } else if (style.code === null) {
-                style.fields[f[2]] = (sumDim>0)? (sumDim +'px') : (i*defDim + '%');
-                sumDim = -1;
-                style.fields[f[3]] = (100-(i+1)*defDim) + '%';
-                style.fields[f[5]] = 'auto';
-            } else if (style.code==='end') { return true; }
-            return false;
-        });
-        
-        // work backwards through the heights
-        sumDim = 0;
-        styles.slice().reverse().some((style, i) => {
-            style.fields[f[3]] = sumDim + 'px';
-            if (style.code === 'end') { 
-                sumDim += style.size + (this.spacing || 0) + (this.spacing || 0);
-                style.fields[f[2]] = 'auto';
-                style.fields[f[5]] = style.size+'px';
-            } else {
-                if (sumDim>0 && style.code !== 'start') {
-                    style.fields[f[5]] = 'auto';
-                }
-                return true; 
-            } 
-            return false;
-        });  
-        return styles;
+    private unitPixel(num:number) { // pattern: [px, px, FILL]
     };
     
     /**
@@ -162,15 +125,13 @@ class Tiles extends Layout {
      * @param components 
      */
     protected getStyles(components:Array<Vnode|Container>):string  { 
-        let f = this.fields;
-        let styles:descriptor[] = this.unit(components.length);
+        let styles = this.unit(components.length);
         components.map((c:Container|Vnode, i:number) => {
-            c.style = `${f[0]}:0%; ${f[1]}:0%; `;
-            Object.keys(styles[i].fields).forEach((st:string) => { c.style += `${st}: ${styles[i].fields[st]};`; });
+            c.style = styles[i];
         });   
-        return this.cssClass;
+        return '.hs-tile-layout';
     };
 };
 
 
-Layout.register('tile', Tiles);
+Layout.register('tiles', Tiles);
