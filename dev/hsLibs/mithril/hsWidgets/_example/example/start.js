a66c78e834e2794ccddf1673b18c6d031ca2264e
hs <>
2017-09-22 19:34:37 -0700

    removed temp files from repo

--- dev/hsLibs/mithril/hsWidgets/_example/example/start.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const _1 = require("../");
-if (_1.Menu) { }
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3RhcnQuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvZXhhbXBsZS9zdGFydC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBLDBCQUEyQjtBQUMzQixFQUFFLENBQUMsQ0FBQyxPQUFJLENBQUMsQ0FBQyxDQUFDLENBQUEsQ0FBQyJ9
--- dev/hsLibs/mithril/hsWidgets/_example/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var hsMenu_1 = require("./hsMenu");
-exports.Menu = hsMenu_1.Menu;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFPQSxtQ0FBMkM7QUFBbEMsd0JBQUEsSUFBSSxDQUFBIn0=
