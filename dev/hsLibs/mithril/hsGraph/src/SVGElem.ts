520cd3100196a2b271cda03e0d827ac515abdf60
hs <>
2017-10-01 19:38:42 -0700

    added clipping and moved defs to SVGElem

--- dev/hsLibs/mithril/hsGraph/src/SVGElem.ts
+++ dev/hsLibs/mithril/hsGraph/src/SVGElem.ts
@@ -1,30 +1,67 @@
 import { m }            from 'hslayout';
 import { Scale }        from './Scale';
-import { Point, Area }  from './Graph';
+
+/** defines a [min-max] range */
+export interface Range {
+    min: number;
+    max: number;
+}
+
+export interface Point {
+    x:   number;
+    y:   number;
+    dx?: number;
+    dy?: number;
+    xunit?: string;
+    yunit?: string;
+    dxunit?: string;
+    dyunit?: string;
+}
+
+export interface Area {
+    w: number;
+    h: number;
+    wunit?: string;
+    hunit?: string;
+}
 
 export interface Style {
-    cssClass?:      string; // the css class to set
-    style?:         string; // the style attributes to set
-    textAnchor?:    string; // horizontal text alignment: 'start' | 'middle' | 'end'
-    baselineShift?: string; // vertical text alignment in 'em'
+    cssClass?:      string;     // the css class to set
+    style?:         string;     // the style attributes to set
+    textAnchor?:    string;     // horizontal text alignment: 'start' | 'middle' | 'end'
+    baselineShift?: string;     // vertical text alignment in 'em'
+}
+
+export interface TextCfg {
+    cssClass?:      string;     // the css class to set
+    hAlign?:        string;     // horizontal text-align: 'start' | 'middle' | 'end'
+    vAlign?:        string;     // vertical align: 'top' | 'center' | 'bottom'
+    hOffset?:       number;     // horizontal label offset in 'em'
+    vOffset?:       number;     // vertical label offset in 'em'
 }
 
 export const round = (num:number) => (''+num).substr(0, 5+(num<0?1:0));
 
 export abstract class SVGElem {
-    text(xy:Point, text:string, style?:Style) {
+    text(xy:Point, text:string, cfg:TextCfg={}) {
+        let baselineShift = '0em';
+        switch(cfg.vAlign) {
+            case 'top':    baselineShift = -0.7 +'em'; break;
+            case 'center': baselineShift = -0.35+'em'; break;
+            case 'bottom': baselineShift =  0   +'em'; break;
+        }
         const param = { 
             x: round(xy.x)     + (xy.xunit||''), 
             y: round(xy.y)     + (xy.yunit||''),
-            dx:round(xy.dx||0) + (xy.dxunit||''),    
-            dy:round(xy.dy||0) + (xy.dyunit||''),
-            class: style? style.cssClass : undefined,
-            style: style? `text-anchor:${style.textAnchor}; baseline-shift: ${style.baselineShift};` : undefined
+            dx:round(cfg.hOffset||0) + 'em',    
+            dy:round(cfg.vOffset||0) + 'em',
+            class: cfg.cssClass,
+            style: `text-anchor:${cfg.hAlign}; baseline-shift: ${baselineShift};`
          };
         return m('text', param, text);
     }
 
-    rect(tl:Point, area:Area, style?:Style) {
+    rect(tl:Point, area:Area, style:Style) {
         const param = {
             x: round(tl.x),       y: round(tl.y),
             width: round(area.w)  + (area.wunit||''), 
@@ -33,6 +70,15 @@ export abstract class SVGElem {
             style: style? `text-anchor:${style.textAnchor}; baseline-shift: ${style.baselineShift}` : undefined
         };
         return m('rect', param);
+    }
+
+    clipRect(tl:Point, area:Area, id:string) {
+        const param = {
+            x: round(tl.x),       y: round(tl.y),
+            width: round(area.w)  + (area.wunit||''), 
+            height: round(area.h) + (area.hunit||'')
+        };
+        return m('defs', m('clipPath', {id: id}, m('rect', param)));
     }
 
     line(from:Point, to:Point, style?:Style) {
@@ -52,9 +98,11 @@ export abstract class SVGElem {
      * @param x the index in each row to use as x coordinate
      * @param y the index in each row to use as y coordinate
      */
-    polyline(data:[number, number][], x:number, y:number, scales:{x:Scale, y:Scale}) {
-        return m('polyline', { points: data.map((p:number[], i:number) => i===0?'': 
-            `${round(scales.x.convert(p[x]))},${round(scales.y.convert(p[y]))}`).join(' ')}); 
+    polyline(data:[number, number][], x:number, y:number, scales:{x:Scale, y:Scale}, id?:string) {
+        return m('polyline', { 
+            'clip-path': id? `url(#${id})` : undefined,
+            points: data.map((p:number[], i:number) => i===0?'': 
+                `${round(scales.x.convert(p[x]))},${round(scales.y.convert(p[y]))}`).join(' ')}); 
     }
 }
 
--- dev/hsLibs/mithril/hsGraph/src/Series.ts
+++ dev/hsLibs/mithril/hsGraph/src/Series.ts
@@ -1,29 +1,53 @@
+/**
+ * # Series
+ * renders the one or more series in a variety of styles.
+ * ### Configurations and Defaults
+ * ```
+ *  title:  {           // the chart title
+ *  }
+ * ```
+ * ### Style classes
+ * ```
+ * styleClasses: {
+ *  }
+ * ```
+ */
 
+/** */
 import { m, Vnode}  from 'hslayout';
 import { Config }   from './Graph';
 import { SVGElem }  from './SVGElem';
 
 
 export interface SeriesSet {
-    data?: Array<number|string>[];   // array of rows; 1st row contains column headers
+    data?: Array<number|string>[];  // array of rows; 1st row contains column headers
+    clip: boolean;                  // clip series to the chart area
     series?: [{ 
-        xHeader:string; 
-        yHeader:string; 
+        xHeader:string;             // the name to index the x values
+        yHeader:string;             // the name to index the y values
     }];
 }
 
 export class Series extends SVGElem { 
+    /** Defines default values for all configurable parameters */
     static config(config:Config) {
         config.series = <SeriesSet>{
             data: null,
+            clip: true,
             series: []
         };
     }
     view(node?: Vnode): Vnode {
-        const cfg       = node.attrs.cfg;
-        const scales    = node.attrs.scales.primary;
-        const data      = cfg.data;
+        const cfg    = node.attrs.cfg;
+        const scales = node.attrs.scales.primary;
+        const data   = cfg.data;
+        const x = scales.x.range[0];
+        const y = scales.y.range[1];
+        const w = scales.x.range[1] - scales.x.range[0];
+        const h = scales.y.range[0] - scales.y.range[1];
+        const id = 'hs'+Math.floor(Math.random()*10000);
         return m('svg', { class:'hs-graph-series'}, [
+            cfg.clip? this.clipRect({x:x, y:y}, {w:w, h:h}, id) : undefined,
             cfg.series.map((s:any) => {
                 const x = data[0].indexOf(s.xHeader);
                 const y = data[0].indexOf(s.yHeader);
@@ -31,7 +55,7 @@ export class Series extends SVGElem {
                     console.log(`${s.xHeader} or ${s.yHeader} not found in ${data[0]}`); 
                     return undefined;
                 } else {
-                    return this.polyline(data, x, y, scales);
+                    return this.polyline(data, x, y, scales, cfg.clip?id:undefined);
                 }
             })
         ]);
