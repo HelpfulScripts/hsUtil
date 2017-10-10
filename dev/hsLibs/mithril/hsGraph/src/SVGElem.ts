60b38591be5971ff2a7b7b3e0e6c98355eea915f
hs <>
2017-10-10 14:41:31 -0700

    shortened default docs, added howLine, verLine

--- dev/hsLibs/mithril/hsGraph/src/SVGElem.ts
+++ dev/hsLibs/mithril/hsGraph/src/SVGElem.ts
@@ -7,6 +7,14 @@ export interface Range {
     max: number;
 }
 
+export interface Rect {
+    nm?:string; // optional name field
+    t:number;   // top
+    l:number;   // left
+    w:number;   // width
+    h:number;   // height
+}
+
 export interface Point {
     x:   number;
     y:   number;
@@ -25,42 +33,77 @@ export interface Area {
     hunit?: string;
 }
 
-export interface Style {
-    cssClass?:      string;     // the css class to set
-    style?:         string;     // the style attributes to set
-    textAnchor?:    string;     // horizontal text alignment: 'start' | 'middle' | 'end'
-    baselineShift?: string;     // vertical text alignment in 'em'
+export interface TitleCfg extends TextCfg {
+    /** determines if the axis title will be rendered */
+    visible:boolean;
+
+    /** the axis title */
+    text: string; 
 }
 
 export interface TextCfg {
-    cssClass?:      string;     // the css class to set
-    hAlign?:        string;     // horizontal text-align: 'start' | 'middle' | 'end'
-    vAlign?:        string;     // vertical align: 'top' | 'center' | 'bottom'
-    hOffset?:       number;     // horizontal label offset in 'em'
-    vOffset?:       number;     // vertical label offset in 'em'
+    /** the css class to set */
+    cssClass?:  string;   
+
+    /** optional absolute x positioning on the canvas, e.g. '50%' */
+    x?:         string;
+
+    /** optional absolute y positioning on the canvas, e.g. '50%' */
+    y?:         string;
+    
+    /** horizontal align: 'start' | 'middle' | 'end'; uses `text-align` attribute */
+    xpos:       string;
+
+    /** vertical align: 'top' | 'center' | 'bottom'; uses `baseline-shift` attribute */
+    ypos:       string;
+
+    /** horizontal label offset in 'em' */
+    hOffset:    number;
+
+    /** vertical label offset in 'em' */
+    vOffset:    number;
 }
 
 export const round = (num:number) => (''+num).substr(0, 5+(num<0?1:0));
 
 export abstract class SVGElem {
-    text(xy:Point, text:string, cfg:TextCfg={}) {
+    /**
+     * plot some text 
+     * @param cfg configures the text alignment and positioning
+     * @param text the text to plot
+     */
+    text(cfg:TextCfg, text:string) {
         let baselineShift = '0em';
-        switch(cfg.vAlign) {
+        let hAlign = cfg.xpos;
+        switch(cfg.xpos) {
+            case 'start':  break;
+            case 'end':    break;
+            case 'middle': 
+            default:       hAlign = 'middle'; break;
+        }
+        switch(cfg.ypos) {
             case 'top':    baselineShift = -0.7 +'em'; break;
             case 'center': baselineShift = -0.35+'em'; break;
-            case 'bottom': baselineShift =  0   +'em'; break;
+            case 'bottom': 
+            default:       baselineShift =  0   +'em'; break;
         }
         const param = { 
-            x: round(xy.x)     + (xy.xunit||''), 
-            y: round(xy.y)     + (xy.yunit||''),
+            x: cfg.x? cfg.x : '',
+            y: cfg.y? cfg.y : '',
             dx:round(cfg.hOffset||0) + 'em',    
             dy:round(cfg.vOffset||0) + 'em',
+            style: `text-anchor:${hAlign}; baseline-shift: ${baselineShift};`,
             class: cfg.cssClass,
-            style: `text-anchor:${cfg.hAlign}; baseline-shift: ${baselineShift};`
-         };
+        };
         return m('text', param, text);
     }
 
+    /**
+     * plot a rectangle in domain coordinates
+     * @param tl the top-left corner of the rect
+     * @param area the width and height of the rect
+     * @param style optional css style setting, such as stroke or stroke-width
+     */
     rect(tl:Point, area:Area, style?:string) {
         const param = {
             x: round(tl.x),       y: round(tl.y),
@@ -71,10 +114,22 @@ export abstract class SVGElem {
         return m('rect', param);
     }
 
+    /**
+     * plot a circle around the center domain point `c`, with radius `r`
+     * @param c the circle's center point in domain coordinates
+     * @param r the circle's radius, in domain coordinates
+     * @param style optional css style setting, such as stroke or stroke-width
+     */
     circle(c:Point, r:number, style?:string) {
         return m('circle', { cx: round(c.x), cy: round(c.y), r: round(r), style: style });
     }
 
+    /**
+     * defines a clip rect to apply to other elelements via the `id`
+     * @param tl top-left corner of the `clipRect` in domain coordinates
+     * @param area width and height of the `clipRect` in domain coordinates
+     * @param id a unique clip id to reference the `clipRect` by
+     */
     clipRect(tl:Point, area:Area, id:string) {
         const param = {
             x: round(tl.x),       y: round(tl.y),
@@ -84,29 +139,93 @@ export abstract class SVGElem {
         return m('defs', m('clipPath', {id: id}, m('rect', param)));
     }
 
-    line(from:Point, to:Point, style?:Style) {
+    /**
+     * plots a straight line from `x0/y0` to `x1/y1`.
+     * @param x0 starting point x domain coordinate 
+     * @param x1 ending point x domain coordinate 
+     * @param y0 starting point y domain coordinate 
+     * @param y1 ending point y domain coordinate 
+     * @param cssClass optional css class attribute
+     */
+    line(x0:number, x1:number, y0:number, y1:number, cssClass?:string) {
         const param = {
-            x1: round(from.x), y1: round(from.y), 
-            x2: round(to.x),   y2: round(to.y), 
-            class: style? style.cssClass : undefined,
+            x1: round(x0), y1: round(y0), 
+            x2: round(x1),   y2: round(y1), 
+            class: cssClass
+        };
+        return m('line', param);
+    }
+
+    /**
+     * plots a horizontal line from `x0/y` to `x1/y`.
+     * @param x0 starting point x domain coordinate 
+     * @param x1 ending point x domain coordinate 
+     * @param y  starting and ending point y domain coordinate 
+     * @param cssClass optional css class attribute
+     */
+    horLine(x0:number, x1:number, y:number, cssClass?:string) {
+        const param = {
+            x1: round(x0), y1: round(y), 
+            x2: round(x1), y2: round(y), 
+            class: cssClass
+        };
+        return m('line', param);
+    }
+
+    /**
+     * plots a vertical line from `x/y0` to `x/y1`.
+     * @param x  starting and ending point x domain coordinate 
+     * @param y0 starting point y domain coordinate 
+     * @param y1 ending point y domain coordinate 
+     * @param cssClass optional css class attribute
+     */
+    verLine(x:number, y0:number, y1:number, cssClass?:string) {
+        const param = {
+            x1: round(x), y1: round(y0), 
+            x2: round(x), y2: round(y1), 
+            class: cssClass
         };
         return m('line', param);
     }
 
     /**
      * plots a polyline from points in `data`. `x` and `y` are the indices to reference 
-     * the data for the x-axis, respectively the y-axis in each row in `data`.
+     * the data for the x-axis, respectively the y-axis in each row in `data`. That is,
+     * plot `data[row][x] / data[row][y]` for all rows. 
      * @param data an array of rows; each row is an array of data. The first row contains the 
      * series names and will be skipped.
      * @param x the index in each row to use as x coordinate
      * @param y the index in each row to use as y coordinate
+     * @param scales the scales to use to convert coordinates into range values
+     * @param id the unique clip-path id to use, or undefined
+     * @param style an optional `style` attribute, e.g. to set the stroke and stroke-width.
      */
     polyline(data:(string | number)[][], x:number, y:number, scales:XYScale, id:string, style?:string) {
         return m('polyline', { 
             'clip-path': id? `url(#${id})` : undefined,
             style: style,
-            points: data.map((p:number[], i:number) => i===0?'': 
-                `${round(scales.x.convert(p[x]))},${round(scales.y.convert(p[y]))}`).join(' ')}); 
+            points: data.map((row:number[], i:number) => i===0?'': 
+                `${round(scales.x.convert(row[x]))},${round(scales.y.convert(row[y]))}`).join(' ')}); 
+    }
+
+    /**
+     * plots a polygon from points in `data`. `x` and `y` are the indices to reference 
+     * the data for the x-axis, respectively the y-axis in each row in `data`. That is,
+     * plot `data[row][x] / data[row][y]` for all rows. 
+     * @param data an array of rows; each row is an array of data. The first row contains the 
+     * series names and will be skipped.
+     * @param x the index in each row to use as x coordinate
+     * @param y the index in each row to use as y coordinate
+     * @param scales the scales to use to convert coordinates into range values
+     * @param id the unique clip-path id to use, or undefined
+     * @param style an optional `style` attribute, e.g. to set the stroke and stroke-width.
+     */
+    polygon(points:[number, number][], id:string, style?:string) {
+        return m('polyline', { 
+            'clip-path': id? `url(#${id})` : undefined,
+            style: style,
+            points: points.map((row:number[]) => 
+                `${round(row[0])},${round(row[1])}`).join(' ')}); 
     }
 }
 
