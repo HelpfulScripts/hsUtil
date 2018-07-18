36ca93671b4fb5f9f249fb3db52ecd3f1114c47b
hs <>
2018-07-18 15:02:00 -0700

    formatting

--- src/Checksum.ts
+++ src/Checksum.ts
@@ -12,5 +12,5 @@
     for (var i = 0; i < len; i++) {
         chk += (s.charCodeAt(i) * (i + 1));
     }
-    return (chk & 0xffffffff).toString(16);
+    return (chk & 0xffffffff).toString(16); 
  }
