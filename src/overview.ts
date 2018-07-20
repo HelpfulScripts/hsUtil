472a12d1d79b50ddc32e44d695bd5f037ce02563
hs <>
2018-07-20 10:18:30 -0700

    updated overview

--- src/overview.ts
+++ src/overview.ts
@@ -12,9 +12,6 @@
  * 
  * ## Index - dependency on [showdown](https://www.npmjs.com/package/showdown)
  * - &nbsp; {@link showdown showdown} an ES6 wrapper for the showdown library
- * 
- * ## Index - dependency on [xlsx](https://www.npmjs.com/package/xlsx)
- * - &nbsp; {@link excel excel} an ES6 wrapper for accessing excel files
  */
 
  /** */
