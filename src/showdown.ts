ec8b845e7ee08748e8010b8b3b7b276742fe7902
hs <>
2018-07-20 10:38:04 -0700

    added comment for showdown

--- src/showdown.ts
+++ src/showdown.ts
@@ -2,6 +2,10 @@ const showdown  = require('showdown');
 
 const converter = new showdown.Converter();
 
+/**
+ * converts `text` to its [showdwon](https://www.npmjs.com/package/showdown) equivalent
+ * @param text the text to convert
+ */
 export function markDown(text:string) { 
     return converter.makeHtml(text);
 }
